87ef60a891e63e81dc0133a9e4506f0ff1df2a60
Nick Snyder <nasnyder@bechtel.com>
2022-04-28 16:42:10 -0700

    formatted stylesheets

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
 var date = new Date();
-console.log(date);
-
-/* rusty on js...shouldn't the date var be *in* the function
-** scoping/global?  I though global vars were frowned upon
-*/
 
 // Function Calendar
 function calendar() {
-  var month = date.getMonth();
-  console.log(`var month is: ${month}`);
+  const month = date.getMonth();
   const yyyy = date.getFullYear();
-  console.log(`var yyyy is: ${yyyy}`);
 
   // set the date to the first of the month on this var
   date.setDate(1);
 
+  // Get the current date only once; previous code had 1 new date call of each day of month
+  const $curDay = new Date()
   const name_Days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
   const day_Week = [0, 1, 2, 3, 4, 5, 6];
-  var month_number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  const month_number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   const namesMonth = [
     "January",
     "Febuary",
@@ -34,83 +29,58 @@ function calendar() {
   ];
 
   // Last date of the current month
-  const lastDay = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1,
-    0
-  ).getDate();
-  console.log(`var lastDay is: ${lastDay}`);
+  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
   //last date of the previous month
-  const prevLastDay = new Date(
-    date.getFullYear(),
-    date.getMonth(),
-    0
-  ).getDate();
-  console.log(`var prevLastDay is: ${prevLastDay}`);
+  const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
 
   //first day of the current month
   const firstDayIndex = date.getDay();
-  console.log(`var firstDayIndex is: ${firstDayIndex}`);
 
   //last day of the week
-  const lastDayIndex = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1,
-    0
-  ).getDay();
-  console.log(`var lastDayIndex is: ${lastDayIndex}`);
+  const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
 
   // To start from the first day of the week
   const nextDays = 7 - lastDayIndex - 1;
-  console.log(`var nextDays is: ${nextDays}`);
 
   // display name of the month and year
-
-  /* this should find the first element in the document with class `monthname` */
-
+  // this should find the first element in the document with class `monthname` */
   const nameofmonth = document.querySelector(".monthname");
   console.log(`const nameofmonth is: ${nameofmonth} from querySelector`)
   for (var n = 0; n <= 11; n++) {
     if (month_number[n] === month) {
-      nameofmonth.innerHTML = `<div><h1>${namesMonth[month]} ${yyyy}</h1></div>`;
+      nameofmonth.innerHTML = `<div>${namesMonth[month]} ${yyyy}</div>`;
     }
   }
 
   //display name of week days
-  const nameofdays = document.querySelector(".calendar-weekDays");
+  const nameofdays = document.querySelector(".calendar-dayNames");
   console.log(`const nameofdays is: ${nameofdays}`)
-  console.log("using javascript + nameofdays var + div to write days")
   let nod = "";
   for (var j = 0; j <= 6; j++) {
-    nod += `<div class="weekdays">${name_Days[j]}</div>`;
+    nod += `<div>${name_Days[j]}</div>`;
   }
-  console.log(`nod is: ${nod}`)
   nameofdays.innerHTML = nod;
 
   let days = "";
   for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+    days += `<div class="prevDate">${prevLastDay - x + 1}</div>`;
   }
 
-  const displayDays = document.querySelector(".calendar-monthDays");
+  const displayDays = document.querySelector(".calendar-dayNumbers");
   console.log(`const displayDays is: ${displayDays}`)
 
   for (let i = 1; i <= lastDay; i++) {
-    if (
-      i === new Date().getDate() &&
-      date.getMonth() === new Date().getMonth()
-    ) {
-      days += `<div>${i}</div>`;
-      displayDays.innerHTML = days;
+    if (i === $curDay.getDate() && month === $curDay.getMonth() && yyyy === $curDay.getFullYear()) {
+      days += `<div class="curDate">${i}</div>`;
     } else {
       days += `<div>${i}</div>`;
-      displayDays.innerHTML = days;
     }
   }
+  displayDays.innerHTML = days;
 
   for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="next-date">${j}</div>`;
+    days += `<div class="futureDate">${j}</div>`;
     displayDays.innerHTML = days;
   }
 }
